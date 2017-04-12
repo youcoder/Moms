271b0e7dc307baa45361ce201256a2a1ac52f33d
youcoder@hotmail.com <f=ma1984626>
2017-04-12 19:33:30 +0900

    token

---
+++ app/src/main/java/com/sodabodyfit/moms/Common/User.java
@@ -0,0 +1,9 @@
+package com.sodabodyfit.moms.Common;
+
+/**
+ * Created by owner on 4/12/2017.
+ */
+
+public class User {
+    public static String token;
+}
--- app/src/main/java/com/sodabodyfit/moms/Common/UserInfo.java
+++ app/src/main/java/com/sodabodyfit/moms/Common/UserInfo.java
@@ -2,9 +2,9 @@ package com.sodabodyfit.moms.Common;
 
 public class UserInfo {
 
-	public static String token;
-	public static String id;
-	public static String name;
+	public String token;
+	public String id;
+	public String name;
 
 	public UserInfo(){
 
