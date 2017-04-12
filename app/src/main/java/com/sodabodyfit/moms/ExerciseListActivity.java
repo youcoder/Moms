b5c50e85779b1713ba6359acc3c9264c9141bd4e
youcoder@hotmail.com <f=ma1984626>
2017-04-12 15:35:35 +0900

    schema

--- app/src/main/java/com/sodabodyfit/moms/ExerciseListActivity.java
+++ app/src/main/java/com/sodabodyfit/moms/ExerciseListActivity.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 public class ExerciseListActivity extends AppCompatActivity {
 
     private ArrayList<Exercise> lstExercise = new ArrayList<Exercise>();
+    private int workoutId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +33,7 @@ public class ExerciseListActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String title = intent.getStringExtra("workout_title");
-        int workoutId = intent.getIntExtra("workout_id", -1);
+        workoutId = intent.getIntExtra("workout_id", -1);
 
         TextView tvTitle = (TextView)findViewById(R.id.txt_title);
         tvTitle.setText(title);
@@ -49,145 +50,6 @@ public class ExerciseListActivity extends AppCompatActivity {
         recycler.setAdapter(adapter);
 
     }
-
-//    public class PhotoAdapter extends BaseAdapter {
-//
-//        private List<Exercise> m_PhotoList = null;
-//        private LayoutInflater m_Inflater = null;
-//        private Context m_Context = null;
-//
-//        public PhotoAdapter(Context conext, List<Exercise> exerciseList)
-//        {
-//            m_PhotoList = exerciseList;
-//            m_Inflater = LayoutInflater.from(conext);
-//            m_Context = conext;
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return m_PhotoList.size();
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return m_PhotoList.get(position);
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//
-//            if(convertView == null)
-//                convertView = m_Inflater.inflate(R.layout.list_exercise, parent, false);
-//
-//            Exercise curExercise = m_PhotoList.get(position);
-//            ((TextView)convertView.findViewById(R.id.tv_subject)).setText(curExercise.title);
-//
-//            String[] imageIds = curExercise.images.split(",");
-//
-//            if(imageIds.length > 0)
-//                LoadImage(convertView, imageIds[0]);
-//
-//            return convertView;
-//        }
-//
-//        // added by usc on 2017/04/10
-//        public void LoadImage(final View convertView, String imageId)
-//        {
-//            DBEngine dbEngine = new DBEngine(m_Context);
-//            Image imageInfo = dbEngine.getImageInfo(imageId);
-//
-//            if(imageInfo.isInAssets)
-//            {
-//                ImageView image = (ImageView) convertView.findViewById(R.id.img_exercise);
-//                Bitmap bMap = BitmapFactory.decodeFile(getFilesDir() + File.separator + imageInfo.name + ".png");
-//                image.setImageBitmap(bMap);
-//            }
-//            else
-//            {
-//                Retrofit retrofit = new Retrofit.Builder()
-//                        .baseUrl(Constants.BASE_URL)
-//                        .addConverterFactory(GsonConverterFactory.create())
-//                        .build();
-//
-//                ImageAPI service = retrofit.create(ImageAPI.class);
-//
-//                Call<ResponseBody> call = service.getImageRequest("Token token=Z6VXst4ia9f3ayUwrTDVgypT", imageInfo.name);
-//
-//                call.enqueue(new Callback<ResponseBody>() {
-//                    @Override
-//                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                        try {
-//
-//                            Log.d("onResponse", "Response came from server");
-//
-//                            String imageName = response.raw().request().url().queryParameterValue(0);
-//                            boolean bSuccess = DownloadImage(response.body(), convertView, imageName);
-//
-//                            Log.d("onResponse", "Image is downloaded and saved ? " + bSuccess);
-//
-//                        } catch (Exception e) {
-//                            Log.d("onResponse", "There is an error");
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-//                        Log.d("onFailure", t.toString());
-//                    }
-//                });
-//            }
-//        }
-//
-//        private boolean DownloadImage(ResponseBody body, View convertView, String fileName) {
-//
-//            try {
-//                Log.d("DownloadImage", "Reading and writing file");
-//                InputStream in = null;
-//                FileOutputStream out = null;
-//
-//                try {
-//                    in = body.byteStream();
-//                    out = new FileOutputStream(getFilesDir() + File.separator + fileName + ".png");
-//                    int c;
-//
-//                    while ((c = in.read()) != -1) {
-//                        out.write(c);
-//                    }
-//                }
-//                catch (IOException e) {
-//                    Log.d("DownloadImage",e.toString());
-//                    return false;
-//                }
-//                finally {
-//                    if (in != null) {
-//                        in.close();
-//                    }
-//                    if (out != null) {
-//                        out.close();
-//                    }
-//                }
-//
-//                ImageView image = (ImageView) convertView.findViewById(R.id.img_exercise);
-//                Bitmap bMap = BitmapFactory.decodeFile(getFilesDir() + File.separator + fileName + ".png");
-//                image.setImageBitmap(bMap);
-//
-//                DBEngine dbEngine = new DBEngine(m_Context);
-//                dbEngine.updateImageInAssets(fileName);
-//
-//                return true;
-//
-//            } catch (IOException e) {
-//                Log.d("DownloadImage",e.toString());
-//                return false;
-//            }
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -206,7 +68,9 @@ public class ExerciseListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_info) {
-            Intent intent = new Intent(ExerciseListActivity.this, ExerciseActivity.class);
+            Intent intent = new Intent(ExerciseListActivity.this, SchemasActivity.class);
+            intent.putExtra("workout_id", workoutId);
+            intent.putExtra("exercise", lstExercise);
             startActivity(intent);
         }else if (id == android.R.id.home) {
             ExerciseListActivity.this.finish();
--- app/src/main/java/com/sodabodyfit/moms/SchemasActivity.java
+++ app/src/main/java/com/sodabodyfit/moms/SchemasActivity.java
@@ -1,302 +1,160 @@
 package com.sodabodyfit.moms;
 
-import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.sodabodyfit.moms.Common.Constants;
-import com.sodabodyfit.moms.Interface.ImageAPI;
+import com.sodabodyfit.moms.Adapter.SchemasAdapter;
+import com.sodabodyfit.moms.Common.DividerItemDecoration;
 import com.sodabodyfit.moms.Models.Exercise;
-import com.sodabodyfit.moms.Models.Image;
 import com.sodabodyfit.moms.Models.Workout;
 import com.sodabodyfit.moms.Provider.DBEngine;
-import com.sodabodyfit.moms.R;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.GsonConverterFactory;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-
-public class SchemasActivity extends Activity implements View.OnClickListener {
-
-    public static final String WORKOUT_ID_KEY = "workout_id";
-
-    private Typeface m_TypeFace = null;
-    private int m_nWorkoutId = 0;
-
-    public class SchemasInfo
-    {
-        public String _title = "";
-        public String _Set = "";
-        public String _Reps = "";
-        public String _Time = "";
-        public String _Rest = "";
-        public String _Kg = "";
-
-        public SchemasInfo(String title, String set, String reps, String time, String rest, String kg)
-        {
-            _title = title;
-            _Set = set;
-            _Reps = reps;
-            _Time = time;
-            _Rest = rest;
-
-            if(kg == null) _Kg = "";
-            else _Kg = kg;
-        }
-    }
+public class SchemasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schemas);
 
-        m_nWorkoutId = getIntent().getIntExtra(WORKOUT_ID_KEY, 1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
 
-        init();
-    }
+        Typeface m_TypeFace = Typeface.createFromAsset(getAssets(), "fonts/Aleo-Bold.otf");
+        
+        Intent intent = getIntent();
+        int workoutId = intent.getIntExtra("workout_id", -1);
+        ArrayList<Exercise> lstExercise = intent.getParcelableArrayListExtra("exercise");
 
-    private void init()
-    {
-        m_TypeFace = Typeface.createFromAsset(getAssets(), "fonts/Aleo-Bold.otf");
+//        LoadImage(workout.image);
+        LinearLayout llSchemasInfo = (LinearLayout) findViewById(R.id.ll_schemas_info);
+        TextView tvSchemasInfo = (TextView) findViewById(R.id.txt_schemas_info);
+//        if(workout.infoDisplayed) {
+//            tvSchemasInfo.setText(workout.info);
+//            llSchemasInfo.setVisibility(View.VISIBLE);
+//        }
 
-        DBEngine dbEngine = new DBEngine(this);
-        Workout workout = dbEngine.getWorkoutInfo(m_nWorkoutId + 1);
-
-        TextView tv_Schemas = (TextView)findViewById(R.id.tv_schemas);
+        TextView tv_Schemas = (TextView)findViewById(R.id.txt_schemas_label);
         tv_Schemas.setTypeface(m_TypeFace);
-        tv_Schemas.setText(workout.title);
 
-        TextView tv_Schemas_label = (TextView)findViewById(R.id.tv_schemas_label);
-        tv_Schemas_label.setTypeface(m_TypeFace);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
+        recycler.setLayoutManager(layoutManager);
+        recycler.addItemDecoration(new DividerItemDecoration(this));
 
-        TextView tv_SchemasInfo = (TextView) findViewById(R.id.tv_schemas_info);
-        if(workout.infoDisplayed) {
-            tv_SchemasInfo.setText(workout.info);
-            tv_SchemasInfo.setVisibility(View.VISIBLE);
-        }
-        else
-            tv_SchemasInfo.setVisibility(View.GONE);
-
-        LoadImage(workout.image);
-
-        ImageView btnClose = (ImageView) findViewById(R.id.btn_close);
-        btnClose.setOnClickListener(this);
-
-        List<Exercise> exerciseList = dbEngine.getExerciseList(m_nWorkoutId + 1);
-
-        ArrayList<SchemasInfo> schemaInfoList = new ArrayList<SchemasInfo>();
-        for(int i = 0; i < exerciseList.size(); i++)
-        {
-            Exercise exercise = exerciseList.get(i);
-            schemaInfoList.add(new SchemasInfo(exercise.title, exercise.sets, exercise.repetions, exercise.times, exercise.rest, exercise.kg));
-        }
-
-        ListView listView = (ListView)findViewById(R.id.schema_list);
-        listView.setAdapter(new SchemaAdapter(this, schemaInfoList));
+        SchemasAdapter adapter = new SchemasAdapter(SchemasActivity.this, lstExercise);
+        recycler.setAdapter(adapter);
     }
 
-    public void LoadImage(String imageId)
-    {
-        DBEngine dbEngine = new DBEngine(this);
-        Image imageInfo = dbEngine.getImageInfo(imageId);
+//    public class SchemaAdapter extends BaseAdapter
+//    {
+//        Context m_context = null;
+//        ArrayList<SchemasInfo> m_SchemaList = null;
+//
+//        public SchemaAdapter(Context context, ArrayList<SchemasInfo> schemaList)
+//        {
+//            m_context = context;
+//            m_SchemaList = schemaList;
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return m_SchemaList.size();
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return m_SchemaList.get(position);
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//
+//            if(convertView == null)
+//            {
+//                convertView = LayoutInflater.from(m_context).inflate(R.layout.list_schemas, parent, false);
+//                TextView tv_set_label_schema = (TextView)convertView.findViewById(R.id.tv_set_label_schema);
+//                tv_set_label_schema.setTypeface(m_TypeFace);
+//
+//                TextView tv_reps_label_schema = (TextView)convertView.findViewById(R.id.tv_reps_label_schema);
+//                tv_reps_label_schema.setTypeface(m_TypeFace);
+//
+//                TextView tv_time_label_schema = (TextView)convertView.findViewById(R.id.tv_time_label_schema);
+//                tv_time_label_schema.setTypeface(m_TypeFace);
+//
+//                TextView tv_rest_label_schema = (TextView)convertView.findViewById(R.id.tv_rest_label_schema);
+//                tv_rest_label_schema.setTypeface(m_TypeFace);
+//
+//                TextView tv_kg_label_schema = (TextView)convertView.findViewById(R.id.tv_kg_label_schema);
+//                tv_kg_label_schema.setTypeface(m_TypeFace);
+//            }
+//
+//            TextView tv_Title = (TextView)convertView.findViewById(R.id.schema_content);
+//            tv_Title.setText(m_SchemaList.get(position)._title);
+//            tv_Title.setTypeface(m_TypeFace);
+//            TextView tv_set = (TextView)convertView.findViewById(R.id.tv_set_schema);
+//            tv_set.setText(String.valueOf(m_SchemaList.get(position)._Set));
+//            tv_set.setTypeface(m_TypeFace);
+//            TextView tv_reps = (TextView)convertView.findViewById(R.id.tv_reps_schema);
+//            tv_reps.setText(String.valueOf(m_SchemaList.get(position)._Reps));
+//            tv_reps.setTypeface(m_TypeFace);
+//            TextView tv_time = (TextView)convertView.findViewById(R.id.tv_time_schema);
+//            tv_time.setText(String.valueOf(m_SchemaList.get(position)._Time) + " sec");
+//            tv_time.setTypeface(m_TypeFace);
+//            TextView tv_rest = (TextView)convertView.findViewById(R.id.tv_rest_schema);
+//            tv_rest.setText(String.valueOf(m_SchemaList.get(position)._Rest) + " sec");
+//            tv_rest.setTypeface(m_TypeFace);
+//            TextView tv_kg = (TextView)convertView.findViewById(R.id.tv_kg_schema);
+//            tv_kg.setText(String.valueOf(m_SchemaList.get(position)._Kg));
+//            tv_kg.setTypeface(m_TypeFace);
+//
+//            return convertView;
+//        }
+//    }
 
-        if(imageInfo.isInAssets)
-        {
-            ImageView image = (ImageView) findViewById(R.id.schema_pic);
-            Bitmap bMap = BitmapFactory.decodeFile(getFilesDir() + File.separator + imageInfo.name + ".png");
-            image.setImageBitmap(bMap);
-        }
-        else
-        {
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        ActionBar actionBar = getSupportActionBar();
+        getMenuInflater().inflate(R.menu.menu_expand, menu);
 
-            ImageAPI service = retrofit.create(ImageAPI.class);
-
-            Call<ResponseBody> call = service.getImageRequest("Token token=Z6VXst4ia9f3ayUwrTDVgypT", imageInfo.name);
-
-            call.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    try {
-
-                        Log.d("onResponse", "Response came from server");
-
-                        String imageName = response.raw().request().url().queryParameterValue(0);
-                        boolean bSuccess = DownloadImage(response.body(), imageName);
-
-                        Log.d("onResponse", "Image is downloaded and saved ? " + bSuccess);
-
-                    } catch (Exception e) {
-                        Log.d("onResponse", "There is an error");
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    Log.d("onFailure", t.toString());
-                }
-            });
-        }
-    }
-
-    private boolean DownloadImage(ResponseBody body, String fileName) {
-
-        try {
-            Log.d("DownloadImage", "Reading and writing file");
-            InputStream in = null;
-            FileOutputStream out = null;
-
-            try {
-                in = body.byteStream();
-                out = new FileOutputStream(getFilesDir() + File.separator + fileName + ".png");
-                int c;
-
-                while ((c = in.read()) != -1) {
-                    out.write(c);
-                }
-            }
-            catch (IOException e) {
-                Log.d("DownloadImage",e.toString());
-                return false;
-            }
-            finally {
-                if (in != null) {
-                    in.close();
-                }
-                if (out != null) {
-                    out.close();
-                }
-            }
-
-            ImageView image = (ImageView) findViewById(R.id.schema_pic);
-            Bitmap bMap = BitmapFactory.decodeFile(getFilesDir() + File.separator + fileName + ".png");
-            image.setImageBitmap(bMap);
-
-            DBEngine dbEngine = new DBEngine(this);
-            dbEngine.updateImageInAssets(fileName);
-
-            return true;
-
-        } catch (IOException e) {
-            Log.d("DownloadImage",e.toString());
-            return false;
-        }
+        return true;
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-        int nResId = v.getId();
-        switch (nResId)
-        {
-            case R.id.btn_close:
-                onClose();
-                break;
+        if (id == R.id.action_close) {
+            SchemasActivity.this.finish();
         }
-    }
-
-    private void onClose()
-    {
-        onBackPressed();
-    }
-
-    public class SchemaAdapter extends BaseAdapter
-    {
-        Context m_context = null;
-        ArrayList<SchemasInfo> m_SchemaList = null;
-
-        public SchemaAdapter(Context context, ArrayList<SchemasInfo> schemaList)
-        {
-            m_context = context;
-            m_SchemaList = schemaList;
-        }
-
-        @Override
-        public int getCount() {
-            return m_SchemaList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return m_SchemaList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if(convertView == null)
-            {
-                convertView = LayoutInflater.from(m_context).inflate(R.layout.schemas_item, parent, false);
-                TextView tv_set_label_schema = (TextView)convertView.findViewById(R.id.tv_set_label_schema);
-                tv_set_label_schema.setTypeface(m_TypeFace);
-
-                TextView tv_reps_label_schema = (TextView)convertView.findViewById(R.id.tv_reps_label_schema);
-                tv_reps_label_schema.setTypeface(m_TypeFace);
-
-                TextView tv_time_label_schema = (TextView)convertView.findViewById(R.id.tv_time_label_schema);
-                tv_time_label_schema.setTypeface(m_TypeFace);
-
-                TextView tv_rest_label_schema = (TextView)convertView.findViewById(R.id.tv_rest_label_schema);
-                tv_rest_label_schema.setTypeface(m_TypeFace);
-
-                TextView tv_kg_label_schema = (TextView)convertView.findViewById(R.id.tv_kg_label_schema);
-                tv_kg_label_schema.setTypeface(m_TypeFace);
-            }
-
-            TextView tv_Title = (TextView)convertView.findViewById(R.id.schema_content);
-            tv_Title.setText(m_SchemaList.get(position)._title);
-            tv_Title.setTypeface(m_TypeFace);
-            TextView tv_set = (TextView)convertView.findViewById(R.id.tv_set_schema);
-            tv_set.setText(String.valueOf(m_SchemaList.get(position)._Set));
-            tv_set.setTypeface(m_TypeFace);
-            TextView tv_reps = (TextView)convertView.findViewById(R.id.tv_reps_schema);
-            tv_reps.setText(String.valueOf(m_SchemaList.get(position)._Reps));
-            tv_reps.setTypeface(m_TypeFace);
-            TextView tv_time = (TextView)convertView.findViewById(R.id.tv_time_schema);
-            tv_time.setText(String.valueOf(m_SchemaList.get(position)._Time) + " sec");
-            tv_time.setTypeface(m_TypeFace);
-            TextView tv_rest = (TextView)convertView.findViewById(R.id.tv_rest_schema);
-            tv_rest.setText(String.valueOf(m_SchemaList.get(position)._Rest) + " sec");
-            tv_rest.setTypeface(m_TypeFace);
-            TextView tv_kg = (TextView)convertView.findViewById(R.id.tv_kg_schema);
-            tv_kg.setText(String.valueOf(m_SchemaList.get(position)._Kg));
-            tv_kg.setTypeface(m_TypeFace);
-
-            return convertView;
-        }
+        return super.onOptionsItemSelected(item);
     }
 }
