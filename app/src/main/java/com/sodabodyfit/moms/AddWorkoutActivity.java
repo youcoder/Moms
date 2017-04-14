8a0aadd26ad36989249beb8d5a051e939b0cb7ca
youcoder@hotmail.com <f=ma1984626>
2017-04-14 20:01:08 +0900

    change addWorkout

--- app/src/main/java/com/sodabodyfit/moms/AddWorkoutActivity.java
+++ app/src/main/java/com/sodabodyfit/moms/AddWorkoutActivity.java
@@ -1,18 +1,14 @@
 package com.sodabodyfit.moms;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sodabodyfit.moms.Common.ImageLoader;
@@ -20,132 +16,81 @@ import com.sodabodyfit.moms.Models.Exercise;
 import com.sodabodyfit.moms.Models.Workout;
 import com.sodabodyfit.moms.Provider.DBEngine;
 
-import java.util.ArrayList;
+public class AddWorkoutActivity extends AppCompatActivity {
 
-public class AddWorkoutActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Exercise m_Exercise;
+    private Exercise exercise;
+    private EditText etWorkoutSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_workout);
 
-        init();
-    }
-
-    private void init()
-    {
         Intent intent = getIntent();
-        m_Exercise = intent.getParcelableExtra("exercise");
+        exercise = intent.getParcelableExtra("exercise");
 
-        TextView tvCancel = (TextView)findViewById(R.id.tv_cancel);
-        tvCancel.setOnClickListener(this);
+        etWorkoutSubject = (EditText)findViewById(R.id.edt_workout_title);
 
-        TextView tvOK = (TextView)findViewById(R.id.tv_ok);
-        tvOK.setOnClickListener(this);
+        TextView tvExerciseTitle = (TextView)findViewById(R.id.txt_subject);
+        tvExerciseTitle.setText(exercise.title);
 
-        LinearLayout llAdd = (LinearLayout)findViewById(R.id.add_workout);
-        llAdd.setOnClickListener(this);
+        ImageView ivExercise = (ImageView)findViewById(R.id.img_exercise);
+        String[] imageIds = exercise.images.split(",");
+        if(imageIds.length > 0)
+            ImageLoader.LoadImage(AddWorkoutActivity.this, ivExercise, imageIds[0]);
 
-        ListView listView = (ListView)findViewById(R.id.exercise_list);
-
-        ArrayList<Exercise> lstExercise = new ArrayList<Exercise>();
-        lstExercise.add(m_Exercise);
-
-        ExericiseListAdapter adapter = new ExericiseListAdapter(this, lstExercise);
-        listView.setAdapter(adapter);
     }
 
     @Override
-    public void onClick(View v) {
-        int nResId = v.getId();
-        switch (nResId)
-        {
-            case R.id.tv_cancel:
-                onClickCancel();
-                break;
-            case R.id.tv_ok:
-                onClickOK();
-                break;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        getMenuInflater().inflate(R.menu.menu_edit_my_workout, menu);
+
+        return true;
     }
 
-    private void onClickOK()
-    {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_ok) {
+            CreateMyworkout();
+            AddWorkoutActivity.this.finish();
+        }else if (id == android.R.id.home) {
+            AddWorkoutActivity.this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void CreateMyworkout() {
+
         try {
-            EditText etOK = (EditText) findViewById(R.id.et_newtitle);
-            String newWorkoutName = etOK.getText().toString();
+            String newWorkoutName = etWorkoutSubject.getText().toString();
 
             DBEngine dbEngine = new DBEngine(this);
             //boolean bExist = dbEngine.IsExistWorkoutName(newWorkoutName);
 
             //if (bExist) {
 
-                Workout newMyWorkout = new Workout();
-                newMyWorkout.category_id = 1;
-                newMyWorkout.title = newWorkoutName;
-                newMyWorkout.info = "";
-                newMyWorkout.infoDisplayed = false;
-                newMyWorkout.exercises = String.valueOf(m_Exercise.exercise_id);
-                String[] imageIds = m_Exercise.images.split(",");
-                if (imageIds.length > 0) newMyWorkout.image = imageIds[0];
+            Workout newMyWorkout = new Workout();
+            newMyWorkout.category_id = 1;
+            newMyWorkout.title = newWorkoutName;
+            newMyWorkout.info = "";
+            newMyWorkout.infoDisplayed = false;
+            newMyWorkout.exercises = String.valueOf(exercise.exercise_id);
+            String[] imageIds = exercise.images.split(",");
+            if (imageIds.length > 0) newMyWorkout.image = imageIds[0];
 
-                dbEngine.addWorkouts(newMyWorkout);
+            dbEngine.addWorkouts(newMyWorkout);
             //}
         }
         catch (Exception e){
             Log.d("AddWorkout", e.toString());
-        }
-    }
-
-    private void onClickCancel()
-    {
-        onBackPressed();
-    }
-
-    public class ExericiseListAdapter extends BaseAdapter {
-
-        private Context m_Context = null;
-        private ArrayList<Exercise> m_ExerciseList = null;
-
-        public ExericiseListAdapter(Context context, ArrayList<Exercise> exerciseList)
-        {
-            m_Context = context;
-            m_ExerciseList = exerciseList;
-        }
-
-        @Override
-        public int getCount() {
-            return m_ExerciseList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return m_ExerciseList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if(convertView == null){
-                convertView = LayoutInflater.from(m_Context).inflate(R.layout.list_my_workout, parent, false);
-            }
-
-            TextView workoutName = (TextView)convertView.findViewById(R.id.txt_subject);
-            workoutName.setText(m_ExerciseList.get(position).title);
-            ImageView workoutImage = (ImageView)convertView.findViewById(R.id.img_exercise);
-
-            String[] imageIds = m_ExerciseList.get(position).images.split(",");
-            if(imageIds.length > 0)
-                ImageLoader.LoadImage(m_Context, workoutImage, imageIds[0]);
-
-            return convertView;
         }
     }
 }
